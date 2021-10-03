edecb47f87443671a49117b66ced6581d69d99b1
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-10-03 21:20:12 +0530

    Add files via upload

--- Wild.js
+++ Wild.js
@@ -1,1 +1,19 @@
 //https://teachablemachine.withgoogle.com/models/S7_VNRs-d/
+
+//Startclassfication function//
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    })
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/S7_VNRs-d/model.json",modelReady)
+    }
+    
+    //modelReady function//
+    function modelReady(){
+        classifier.classify(gotResults)
+    }
+    
+    // gotResults function//
+    function gotResults(error,results){
+        console.log(gotResults)
+    }
