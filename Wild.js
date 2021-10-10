a31d1c3048168c67f62a54c7c706cdc42af1d52d
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-10-10 20:24:03 +0530

    Add files via upload

--- Wild.js
+++ Wild.js
@@ -15,5 +15,50 @@ function startClassification(){
     
     // gotResults function//
     function gotResults(error,results){
-        console.log(gotResults)
+        // gotResults function//
+function gotResults(error,results){
+    if (error) {
+        console.error(error)
+    } else {
+        random_number_r = Math.ceil(Math.random()*255) + 1
+        random_number_g = Math.ceil(Math.random()*255) + 1
+        random_number_b = Math.ceil(Math.random()*255) + 1
+        document.getElementById("result_label").innerHTML= "I can hear- " + results[0].label
+        document.getElementById("result_confidence").innerHTML= "Accuracy- " + (results[0].confidence*100).toFixed(2) + " %"
+        document.getElementById("result_label").style.color="rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")"
+        document.getElementById("result_confidence").style.color="rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")"
+ 
+        //Variable//
+         img=document.getElementById("Crow")
+         img2=document.getElementById("WOLF")
+         img3=document.getElementById("Dog")
+         img4=document.getElementById("Elephant")
+ 
+         // Noise works//
+     if (results[0].label== "Crow") {
+         img.src="Crowy.gif"
+         img2.src="Dog.jpg"
+         img3.src="WOLF.jpg"
+         img4.src="Elephant.jpg"
+     } else if(results[0].label== "Dog"){
+        img.src="Crow.jpg"
+        img2.src="Doggy.gif"
+        img3.src="WOLF.jpg"
+        img4.src="Elephant.jpg"
+     }
+     else if(results[0].label== "Elephant"){
+        img.src="Crow.jpg"
+        img2.src="Dog.jpg"
+        img3.src="WOLF.jpg"
+        img4.src="Elly.gif"
+     }
+     else {
+        img.src="Crow.jpg"
+        img2.src="Dog.jpg"
+        img3.src="wolf-howl.gif"
+        img4.src="Elephant.jpg"
+     }
+    }
+ }
+ 
     }
